52d872940579ab52c16530ae992ade8701169386
LordEatch <113691768+LordEatch@users.noreply.github.com>
2025-05-05 15:52:08 +0100

    fix: removed non-default library dependencies from config.py

--- uni_cal_pro/config.py
+++ uni_cal_pro/config.py
@@ -1,5 +1,4 @@
 import os
-import pkg_resources
 import subprocess
 import sys
 
@@ -9,36 +8,33 @@ CONFIG_DIRECTORY_PATH = "config"
 REQUIREMENTS_FILENAME = "requirements.txt"
 
 
-def package_installed(package_name):
-    """Check if a package is already installed."""
-
-    installed_packages = {pkg.key for pkg in pkg_resources.working_set}
-    return package_name.lower() in installed_packages
-
-def install_package(package_name):
-    if package_installed(package_name):
-        raise Exception(f"Package '{package_name}' is already installed.")
-
-    try:
-        print(f"Installing package: {package_name}")
-        subprocess.check_call([sys.executable, "-m", "pip", "install", package_name])
-        print(f"Package '{package_name}' installed successfully.")
-    except subprocess.CalledProcessError as e:
-        print(f"Failed to install package '{package_name}'. Error: {e}")
-
-def get_required_packages(filename):
-    with open(filename) as requirements:
-        return [line.strip() for line in requirements.readlines()]
+def install_package(pypi_name):
+    """Installs a package using pip."""
+    print(f"Installing {pypi_name}...")
+    subprocess.check_call([sys.executable, "-m", "pip", "install", pypi_name])
 
 def install_missing_packages():
-    requirements_path = os.path.join(CONFIG_DIRECTORY_PATH, REQUIREMENTS_FILENAME)
+    requirements_filepath = os.path.join(CONFIG_DIRECTORY_PATH, REQUIREMENTS_FILENAME)
 
-    requirements = get_required_packages(requirements_path)
+    with open(requirements_filepath) as requirements:
+        for requirement in requirements:
+            requirement = requirement.strip()
 
-    for package in requirements:
-        if not package_installed(package):
-            print(f"\tInstalling package: {package}...")
-            install_package(package)
+            # Skip empty lines.
+            if not requirement:
+                continue
+
+            try:
+                pypi_name, import_name = [x.strip() for x in requirement.split(",")]
+            except ValueError:
+                print(f"Skipping malformed line: {requirement}")
+                continue
+
+            try:
+                __import__(import_name)
+                print(f"{import_name} is already installed.")
+            except ImportError:
+                install_package(pypi_name)
 
 # NOTE This runs every time this module is imported. It needs to be this way so that missing packages are installed prior to attempting to import them in main.py.
 install_missing_packages()
